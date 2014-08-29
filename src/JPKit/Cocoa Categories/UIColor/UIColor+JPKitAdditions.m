c87ebf88685ec82e86f503753fc5021c06ffa53c
JP McGlone <jpmcglone@users.noreply.github.com>
2014-08-29 13:22:01 -0700

    cleaner formatting of UIColor crayola colors

--- src/JPKit/Cocoa Categories/UIColor/UIColor+JPKitAdditions.m
+++ src/JPKit/Cocoa Categories/UIColor/UIColor+JPKitAdditions.m
@@ -52,85 +52,23 @@
 
 #pragma mark - Crayola Colors
 #pragma mark - 8 pack
-+ (UIColor *)jp_blackColor
-{
-    return UIColorFromHex(0x000000, 1.0);
-}
-
-+ (UIColor *)jp_blueColor
-{
-    return UIColorFromHex(0x1F75FE, 1.0);
-}
-
-+ (UIColor *)jp_brownColor
-{
-    return UIColorFromHex(0xB4674D, 1.0);
-}
-
-+ (UIColor *)jp_greenColor
-{
-    return UIColorFromHex(0x1CAC78, 1.0);
-}
-
-+ (UIColor *)jp_orangeColor
-{
-    return UIColorFromHex(0xFF7538, 1.0);
-}
-
-+ (UIColor *)jp_redColor
-{
-    return UIColorFromHex(0xEE204D, 1.0);
-}
-
-+ (UIColor *)jp_violetColor
-{
-    return UIColorFromHex(0x926EAE, 1.0);
-}
-
-+ (UIColor *)jp_yellowColor
-{
-    return UIColorFromHex(0xFCE883, 1.0);
-}
++ (UIColor *)jp_blackColor{ return UIColorFromHex(0x000000, 1.0); }
++ (UIColor *)jp_blueColor{ return UIColorFromHex(0x1F75FE, 1.0); }
++ (UIColor *)jp_brownColor{ return UIColorFromHex(0xB4674D, 1.0); }
++ (UIColor *)jp_greenColor{ return UIColorFromHex(0x1CAC78, 1.0); }
++ (UIColor *)jp_orangeColor{ return UIColorFromHex(0xFF7538, 1.0); }
++ (UIColor *)jp_redColor{ return UIColorFromHex(0xEE204D, 1.0); }
++ (UIColor *)jp_violetColor{ return UIColorFromHex(0x926EAE, 1.0); }
++ (UIColor *)jp_yellowColor{ return UIColorFromHex(0xFCE883, 1.0); }
 
 #pragma mark - 16 pack
-+ (UIColor *)jp_blueGreenColor
-{
-    return UIColorFromHex(0x0D98BA, 1.0);
-}
-
-+ (UIColor *)jp_blueVioletColor
-{
-    return UIColorFromHex(0x7366BD, 1.0);
-}
-
-+ (UIColor *)jp_carnationPinkColor
-{
-    return UIColorFromHex(0xFFAACC, 1.0);
-}
-
-+ (UIColor *)jp_redOrangeColor
-{
-    return UIColorFromHex(0xFF5349, 1.0);
-}
-
-+ (UIColor *)jp_redVioletColor
-{
-    return UIColorFromHex(0xC0448F, 1.0);
-}
-
-+ (UIColor *)jp_whiteColor
-{
-    return UIColorFromHex(0xFFFFFF, 1.0);
-}
-
-+ (UIColor *)jp_yellowGreenColor
-{
-    return UIColorFromHex(0xC5E384, 1.0);
-}
-
-+ (UIColor *)jp_yellowOrangeColor
-{
-    return UIColorFromHex(0xFFAE42, 1.0);
-}
++ (UIColor *)jp_blueGreenColor{ return UIColorFromHex(0x0D98BA, 1.0); }
++ (UIColor *)jp_blueVioletColor{ return UIColorFromHex(0x7366BD, 1.0); }
++ (UIColor *)jp_carnationPinkColor{ return UIColorFromHex(0xFFAACC, 1.0); }
++ (UIColor *)jp_redOrangeColor{ return UIColorFromHex(0xFF5349, 1.0); }
++ (UIColor *)jp_redVioletColor{ return UIColorFromHex(0xC0448F, 1.0); }
++ (UIColor *)jp_whiteColor{ return UIColorFromHex(0xFFFFFF, 1.0); }
++ (UIColor *)jp_yellowGreenColor{ return UIColorFromHex(0xC5E384, 1.0); }
++ (UIColor *)jp_yellowOrangeColor{ return UIColorFromHex(0xFFAE42, 1.0); }
 
 @end
