96df062ba5ba2749f7b10eebc86d953905fa7ca4
JP McGlone <jp@trifl.co>
2014-12-31 12:58:06 -0800

    created way to do an action after the tableview scroll animation is done

---
+++ src/JPKit/Cocoa Categories/UITableView/UITableView+UITableView_JPKitAdditions.h
@@ -0,0 +1,19 @@
+//
+//  UITableView+UITableView_JPKitAdditions.h
+//  JPKit
+//
+//  Created by JP McGlone on 12/31/14.
+//  Copyright (c) 2014 JP McGlone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (UITableView_JPKitAdditions)
+
+-(void)jp_scrollToRowAtIndexPath:(NSIndexPath *)indexPath
+                atScrollPosition:(UITableViewScrollPosition)scrollPosition
+                        animated:(BOOL)animated
+                  scrollFinished:(void (^)())scrollFinished;
+-(void)jp_scrollToTopAnimated:(BOOL)animated scrollFinished:(void (^)())scrollFinished;
+
+@end
---
+++ src/JPKit/Cocoa Categories/UITableView/UITableView+UITableView_JPKitAdditions.m
@@ -0,0 +1,40 @@
+//
+//  UITableView+UITableView_JPKitAdditions.m
+//  JPKit
+//
+//  Created by JP McGlone on 12/31/14.
+//  Copyright (c) 2014 JP McGlone. All rights reserved.
+//
+
+#import "UITableView+UITableView_JPKitAdditions.h"
+
+@implementation UITableView (UITableView_JPKitAdditions)
+
+-(void)jp_scrollToTopAnimated:(BOOL)animated scrollFinished:(void (^)())scrollFinished
+{
+    [CATransaction begin];
+    [CATransaction setCompletionBlock:scrollFinished];
+
+    [self beginUpdates];
+    [self setContentOffset:CGPointZero animated:animated];
+    [self endUpdates];
+
+    [CATransaction commit];
+}
+
+-(void)jp_scrollToRowAtIndexPath:(NSIndexPath *)indexPath
+             atScrollPosition:(UITableViewScrollPosition)scrollPosition
+                     animated:(BOOL)animated
+               scrollFinished:(void (^)())scrollFinished
+{
+    [CATransaction begin];
+    [CATransaction setCompletionBlock:scrollFinished];
+
+    [self beginUpdates];
+    [self scrollToRowAtIndexPath:indexPath atScrollPosition:scrollPosition animated:animated];
+    [self endUpdates];
+
+    [CATransaction commit];
+}
+
+@end
